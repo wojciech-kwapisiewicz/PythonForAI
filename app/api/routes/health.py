754dd28b7739ae91bfc10ade20b7bff41694be17
Wojciech Kwapisiewicz <wojciech.kwapisiewicz@gmail.com>
2026-02-08 19:45:20 +0100

    Added health router

---
+++ app/api/routes/health.py
@@ -0,0 +1,9 @@
+# app/api/routes/health.py
+from fastapi import APIRouter
+
+router = APIRouter()
+
+
+@router.get("/health")
+def health():
+    return {"status": "ok"}
--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 # app/main.py
 from fastapi import FastAPI
-# from app.api.routes import weather
+from app.api.routes import health
 
 app = FastAPI()
 # app.include_router(weather.router)
+
+app.include_router(health.router)
 
 
 @app.get("/")
